060ca52134b14a65b4f3e197bb173948d808e3e3
JaeSeoKim <devjaeseo@gmail.com>
2022-04-18 21:46:33 +0900

    feat(blog): 전역 스타일 breakpoint 추가 #39

--- packages/blog/styles/theme.ts
+++ packages/blog/styles/theme.ts
@@ -10,6 +10,14 @@ const color = {
   grey400: '#737373',
 } as const
 
+export const breakpoint = {
+  sm: '640px',
+  md: '768px',
+  lg: '1024px',
+  xl: '1280px',
+  xxl: '1536px',
+} as const
+
 export const theme = {
   color,
 }
