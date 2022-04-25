ad3c21131ddd10fdcf4ee22794cd342d9f190399
Yenowme <76904042+Yenowme@users.noreply.github.com>
2022-04-25 22:27:06 +0900

    style(blog): card 컴포넌트 스타일린트 적용

--- packages/blog/components/Card/Card.styled.tsx
+++ packages/blog/components/Card/Card.styled.tsx
@@ -1,39 +1,44 @@
-import { theme } from 'react-craft-x/src/theme'
 import styled from 'styled-components'
 
 const Styled = {
   rootContainer: styled.section`
+    padding-bottom: 16px;
+
+    overflow: hidden;
+
     background-color: ${({ theme }) => theme.color.white};
     border-radius: 3px;
     box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.25);
-    padding-bottom: 16px;
-    overflow: hidden;
   `,
   img: styled.img`
-    object-fit: cover;
     width: 100%;
+
+    object-fit: cover;
   `,
   title: styled.h1`
     margin-top: 15px;
+    margin-bottom: 6px;
     margin-left: 16px;
+
     font-weight: 700;
     font-size: 20px;
     line-height: 24px;
-    margin-bottom: 6px;
   `,
   description: styled.p`
-    margin-left: 16px;
     margin-right: 16px;
+    margin-bottom: 40px;
+    margin-left: 16px;
+
     color: ${({ theme }) => theme.color.grey400};
     font-weight: 400;
     font-size: 16px;
     line-height: 19px;
-    margin-bottom: 40px;
-    word-break: break-all;
     white-space: pre-wrap;
+    word-break: break-all;
   `,
   date: styled.p`
     margin-left: 16px;
+
     color: ${({ theme }) => theme.color.grey400};
     font-weight: 400;
     font-size: 10px;
