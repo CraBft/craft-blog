4f44854c132db6f2a9199eb5b0e76e7ec91a6847
Likilee <gnsdlrl@daum.net>
2022-04-26 17:26:28 +0900

    feat(extension): 컴포넌트 추가 Card
    - 이후 더 적절한 이름으로 변경 필요할듯합니다.

---
+++ packages/extension/src/pages/Home/components/Card/Card.Styled.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+import { HiArrowRight } from 'react-icons/hi'
+
+export const StyledCard = styled.section`
+  border: 1px solid #000000;
+`
+
+export const StyledLogo = styled.img``
+
+export const StyledNextIcon = styled(HiArrowRight)``
+
+export const StyledTitle = styled.h1``
+
+export const StyledDescription = styled.div``
