f013821bd771ce49e7978d38b9add6051e09ce14
Likilee <gnsdlrl@daum.net>
2022-04-26 17:29:35 +0900

    feat(extension): 리액트 라우터 App에 적용

--- packages/extension/src/app.tsx
+++ packages/extension/src/app.tsx
@@ -1,57 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import craftXIconSrc from './craftx-icon.png'
+import { Route, MemoryRouter, Routes } from 'react-router-dom'
+import BlogConfig from './pages/BlogConfig'
+import Home from './pages/Home'
 
 const App: React.FC<{}> = () => {
-  const isDarkMode = useCraftDarkMode()
-
-  React.useEffect(() => {
-    if (isDarkMode) {
-      document.body.classList.add('dark')
-    } else {
-      document.body.classList.remove('dark')
-    }
-  }, [isDarkMode])
-
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-      }}
-    >
-      <img className="icon" src={craftXIconSrc} alt="CraftX logo" />
-      <button className={`btn ${isDarkMode ? 'dark' : ''}`} onClick={insertHelloWorld}>
-        Hello world!
-      </button>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog-config" element={<BlogConfig />} />
+      </Routes>
+    </>
   )
-}
-
-function useCraftDarkMode() {
-  const [isDarkMode, setIsDarkMode] = React.useState(false)
-
-  React.useEffect(() => {
-    craft.env.setListener((env) => setIsDarkMode(env.colorScheme === 'dark'))
-  }, [])
-
-  return isDarkMode
-}
-
-function insertHelloWorld() {
-  const block = craft.blockFactory.textBlock({
-    content: 'Hello world!',
-  })
-
-  craft.dataApi.addBlocks([block])
 }
 
 export function initApp() {
   const appNode = document.getElementById('react-root')
   ReactDOM.createRoot(appNode!).render(
     <React.StrictMode>
-      <App />
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
     </React.StrictMode>
   )
 }
