dc6686521f243318adc872a822b4b197646c0851
Likilee <gnsdlrl@daum.net>
2022-04-28 22:09:58 +0900

    feat(context): blogInfoContext 와 BlogLoadStateContext 하나로 통합 진행

--- packages/extension/src/app.tsx
+++ packages/extension/src/app.tsx
@@ -3,19 +3,18 @@ import ReactDOM from 'react-dom/client'
 import { Route, MemoryRouter, Routes } from 'react-router-dom'
 import BlogConfig from './pages/BlogConfig'
 import Home from './pages/Home'
-import { BlogInfoContextProvider, BlogLoadStateContextProvider } from './contexts/blogGlobalContext'
+import { BlogInfoContextProvider } from './contexts/blogGlobalContext'
 import Publish from './pages/Publish'
+
 const App: React.FC<{}> = () => {
   return (
-    <BlogLoadStateContextProvider>
-      <BlogInfoContextProvider>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/blog-config" element={<BlogConfig />} />
-          <Route path="/publish" element={<Publish />} />
-        </Routes>
-      </BlogInfoContextProvider>
-    </BlogLoadStateContextProvider>
+    <BlogInfoContextProvider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog-config" element={<BlogConfig />} />
+        <Route path="/publish" element={<Publish />} />
+      </Routes>
+    </BlogInfoContextProvider>
   )
 }
 
--- packages/extension/src/pages/Home/Home.tsx
+++ packages/extension/src/pages/Home/Home.tsx
@@ -1,9 +1,9 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import Card from './components/Card'
 import NavBar from '../../components/NavBar'
-import { GithubBlogInfo } from '../../utils/craftStorageApi'
-import { BlogLoadStateContext } from '../../contexts/blogGlobalContext'
-import { Navigate, useNavigate } from 'react-router-dom'
+import craftStorageApi, { GithubBlogInfo } from '../../utils/craftStorageApi'
+import { useBlogInfoContext } from '../../contexts/blogGlobalContext'
+import { Navigate } from 'react-router-dom'
 
 export type HomeProps = {
   blogInfo: GithubBlogInfo
@@ -27,16 +27,19 @@ function Home(props: HomeProps): JSX.Element {
 // 다른 컴포넌트들에도 재사용하려면 상태에 따라서 일관된 동작을 정의해야될거 같은데 가능할까?..
 const withBlogLoad = (Component: typeof Home) => {
   const Wrapper = (props: any) => {
-    const [loadState, setLoadState] = useContext(BlogLoadStateContext)
+    const { getBlogInfo, loadStatus } = useBlogInfoContext()
 
+    useEffect(() => {
+      getBlogInfo()
+    }, [getBlogInfo])
     // useEffect(() => {
     //   setTimeout(() => {
-    //     setLoadState('success')
+    //     setLoadStatus('success')
     //   }, 1000)
-    // }, [setLoadState])
+    // }, [setLoadStatus])
 
-    if (loadState === 'loading') return <>loading...</>
-    else if (loadState === 'fail') return <Component {...props} />
+    if (loadStatus === 'loading') return <>loading...</>
+    else if (loadStatus === 'fail') return <Component {...props} />
     return <Navigate to="/publish" />
   }
   return Wrapper
