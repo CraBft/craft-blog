10f92407558c696a405092a21dfb41711dcf47c4
Likilee <gnsdlrl@daum.net>
2022-05-12 14:07:44 +0900

    chore(extension): js-base64 패키지 추가
    - Github Api 에서 putContents 실행 시, 컨텐츠를 base64로 인코딩하여 전송해야 해서
      base64 인코더 라이브러리 추가
    
    - 내장 API는 Webview에서 정상 동작하지 않아서 외부 라이브러리 사용 결정

--- packages/extension/src/pages/Publish/Publish.tsx
+++ packages/extension/src/pages/Publish/Publish.tsx
@@ -1,7 +1,53 @@
 import React from 'react'
+import { useBlogInfoContext } from '../../contexts/blogGlobalContext'
+import { putContents, getContents } from '../../utils/githubApi'
+import Button from '../BlogConfig/components/Button'
+import Base64 from 'js-base64'
 
 export type PublishProps = {}
 
 export default function Publish(): JSX.Element {
-  return <div>publish</div>
+  const {
+    blogInfo: { apiToken, repoFullName },
+  } = useBlogInfoContext()
+
+  const onClick = async () => {
+    const page = await craft.dataApi.getCurrentPage()
+    if (page.status === 'error') {
+      console.error('error')
+      return
+    }
+
+    const preContents = await getContents(apiToken, {
+      repoFullName,
+      path: '.github/temp/craft_raw.json',
+    })
+    console.log(preContents)
+
+    const content = Base64.encode(JSON.stringify(page))
+
+    const sha = (() => {
+      if (preContents.status !== 200) return
+      if (Array.isArray(preContents.body)) {
+        return preContents.body[0].sha
+      }
+      return preContents.body.sha
+    })()
+
+    console.log(
+      await putContents(apiToken, {
+        repoFullName,
+        path: '.github/temp/craft_raw.json',
+        message: 'commit msg!',
+        content,
+        sha: sha,
+      })
+    )
+  }
+
+  return (
+    <>
+      <Button onClick={onClick}>publish!!</Button>
+    </>
+  )
 }
