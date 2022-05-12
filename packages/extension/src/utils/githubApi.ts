a2b0cf255cd5a7e2f0159c5dcacedb6e86b5495c
Likilee <gnsdlrl@daum.net>
2022-05-12 14:05:04 +0900

    feat(extension): githubApi - getContents, putContents 추가

--- packages/extension/src/utils/githubApi.ts
+++ packages/extension/src/utils/githubApi.ts
@@ -45,7 +45,63 @@ export async function getUser(personalAccessToken: string) {
     url: `${END_POINT}/user`,
     token: personalAccessToken,
   })
-  return await response.data?.body?.json()
+  return response.data?.body?.json()
+}
+
+type getContentsParameters = Omit<
+  Omit<Endpoints['GET /repos/{owner}/{repo}/contents/{path}']['parameters'], 'owner'>,
+  'repo'
+> & {
+  repoFullName: string
+}
+
+type getContentsResponse = Endpoints['GET /repos/{owner}/{repo}/contents/{path}']['response']['data']
+
+export async function getContents(
+  personalAccessToken: string,
+  { repoFullName, path, ...params }: getContentsParameters
+) {
+  const response = await client<getContentsResponse>({
+    method: 'GET',
+    url: `${END_POINT}/repos/${repoFullName}/contents/${path}?${Object.entries(params)
+      .map(([key, value]) => `${encodeURI(key)}=${encodeURI(value)}`)
+      .join('&')}`,
+    token: personalAccessToken,
+  })
+
+  const { status, headers, body } = response.data!
+
+  return {
+    status,
+    headers,
+    body: await body!.json(),
+  }
+}
+
+type putContentsParameters = Omit<
+  Omit<Endpoints[`PUT /repos/{owner}/{repo}/contents/{path}`]['parameters'], 'owner'>,
+  'repo'
+> & {
+  repoFullName: string
+}
+
+type putContentsResponse = Endpoints[`PUT /repos/{owner}/{repo}/contents/{path}`]['response']['data']
+
+export async function putContents(
+  personalAccessToken: string,
+  { repoFullName, path, ...params }: putContentsParameters
+) {
+  const response = await client<putContentsResponse>({
+    method: 'PUT',
+    url: `${END_POINT}/repos/${repoFullName}/contents/${path}`,
+    token: personalAccessToken,
+    body: {
+      type: 'text',
+      text: JSON.stringify(params),
+    },
+  })
+
+  return response.data
 }
 
 type UserRepoResponse = Endpoints[`GET /user/repos`]['response']['data']
